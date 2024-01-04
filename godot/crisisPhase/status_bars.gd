a6d3cd2f829fcec8ce3734921fa4066cfb4036e1
cccynic <karina.miroslavova.lazarova@gmail.com>
2024-01-04 12:41:39 +0100

    EG v4
    added summary of all actions

--- godot/crisisPhase/status_bars.gd
+++ godot/crisisPhase/status_bars.gd
@@ -17,8 +17,10 @@ func _on_avatar_nutrition_updated(p_new_value: int) -> void:
 	$VBoxContainer/HBoxContainer2/NutritionBar.set_value_no_signal(perc)
 
 func _on_avatar_hydration_updated(p_new_value: int) -> void:
-	$VBoxContainer/HBoxContainer/HydrationBar.set_value_no_signal(p_new_value)
+	var perc: int = float(p_new_value) / EMC_Avatar.MAX_VITALS * 100 #float() Casting wichtig!
+	$VBoxContainer/HBoxContainer/HydrationBar.set_value_no_signal(perc)
 
 func _on_avatar_health_updated(p_new_value: int) -> void:
-	$VBoxContainer/HBoxContainer3/HealthBar.set_value_no_signal(p_new_value)
+	var perc: int = float(p_new_value) / EMC_Avatar.MAX_VITALS * 100 #float() Casting wichtig!
+	$VBoxContainer/HBoxContainer3/HealthBar.set_value_no_signal(perc)
 
