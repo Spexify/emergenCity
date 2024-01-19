2810ba96f48c1906138fc4408e01a34979c4b415
Ole Fabritz <o.fabritz@web.de>
2024-01-19 13:39:35 +0100

    Rainwaterbarrel gui polish v2

--- godot/GUI/actionGUI/rainwater_barrel_GUI.gd
+++ godot/GUI/actionGUI/rainwater_barrel_GUI.gd
@@ -6,7 +6,7 @@ var _inventory_ref : EMC_Inventory
 func setup(p_overworld_states_mngr_ref: EMC_OverworldStatesMngr, p_inventory_ref: EMC_Inventory) -> void:
 	_overworld_states_mngr_ref = p_overworld_states_mngr_ref
 	_inventory_ref = p_inventory_ref
-	$NinePatchRect/VBoxContainer/PanelContainer/RichTextLabel.text = "In der Regentonne sind " + str(float(_overworld_states_mngr_ref.get_furniture_state(EMC_OverworldStatesMngr.Furniture.RAINWATER_BARREL)) / 4) + "l Wasser."
+	$PanelContainer/VBoxContainer/PanelContainer/RichTextLabel.text = "In der Regentonne sind " + str(float(_overworld_states_mngr_ref.get_furniture_state(EMC_OverworldStatesMngr.Furniture.RAINWATER_BARREL)) / 4) + "l Wasser."
 
 func _on_get_water_pressed() -> void:
 	if _overworld_states_mngr_ref.get_furniture_state(EMC_OverworldStatesMngr.Furniture.RAINWATER_BARREL) > 0:
@@ -15,7 +15,7 @@ func _on_get_water_pressed() -> void:
 				EMC_OverworldStatesMngr.Furniture.RAINWATER_BARREL,
 				_overworld_states_mngr_ref.get_furniture_state(EMC_OverworldStatesMngr.Furniture.RAINWATER_BARREL) - 1
 			)
-			$NinePatchRect/VBoxContainer/PanelContainer/RichTextLabel.text = "In der Regentonne sind " + str(float(_overworld_states_mngr_ref.get_furniture_state(EMC_OverworldStatesMngr.Furniture.RAINWATER_BARREL)) / 4) + "l Wasser."
+			$PanelContainer/VBoxContainer/PanelContainer/RichTextLabel.text = "In der Regentonne sind " + str(float(_overworld_states_mngr_ref.get_furniture_state(EMC_OverworldStatesMngr.Furniture.RAINWATER_BARREL)) / 4) + "l Wasser."
 
 
 func _on_done_btn_pressed() -> void:
@@ -23,5 +23,5 @@ func _on_done_btn_pressed() -> void:
 	closed.emit()
 	
 func show_gui(p_action: EMC_Action) -> void:
-	$NinePatchRect/VBoxContainer/PanelContainer/RichTextLabel.text = "In der Regentonne sind " + str(float(_overworld_states_mngr_ref.get_furniture_state(EMC_OverworldStatesMngr.Furniture.RAINWATER_BARREL)) / 4) + "l Wasser."
+	$PanelContainer/VBoxContainer/PanelContainer/RichTextLabel.text = "In der Regentonne sind " + str(float(_overworld_states_mngr_ref.get_furniture_state(EMC_OverworldStatesMngr.Furniture.RAINWATER_BARREL)) / 4) + "l Wasser."
 	show()
