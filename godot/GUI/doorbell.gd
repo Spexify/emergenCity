992aebbf82485cf2f43de12a3ffbead5c48b973b
marCOmics <48869977+marCOmics@users.noreply.github.com>
2024-01-24 23:19:32 +0100

    Apartment GUI v1.0
    + Added Apartment doorbells GUI (currently connected to Market Stage as a placeholder)

---
+++ godot/GUI/doorbell.gd
@@ -0,0 +1,29 @@
+extends TextureButton
+
+signal rang(p_stage_change_ID: EMC_Action.IDs)
+
+const POS_NORMAL := Vector2(0, 20)
+const POS_PRESSED := Vector2(0, 30)
+
+var _stage_change_ID: EMC_Action.IDs
+
+########################################### PUBLIC METHODS #########################################
+func setup(p_text: String, p_stage_change_ID: EMC_Action.IDs) -> void:
+	$RTL.text = "[center][i]" + p_text + "[/i][/center]"
+	_stage_change_ID = p_stage_change_ID
+
+
+########################################## PRIVATE METHODS #########################################
+func _on_button_down() -> void:
+	$RTL.position = POS_PRESSED
+
+
+func _on_button_up() -> void:
+	$RTL.position = POS_NORMAL
+
+
+func _on_pressed() -> void:
+	$DingDongSFX.seek(1.5) #Play to second x
+	$DingDongSFX.play()
+	#await $DingDongSFX.finished
+	rang.emit(_stage_change_ID)
---
+++ godot/GUI/doorbells_GUI.gd
@@ -0,0 +1,41 @@
+extends EMC_GUI
+
+const _DOORBELL_SCN: PackedScene = preload("res://GUI/doorbell.tscn")
+
+@onready var _doorbell_list := $PanelCont/MarginCont/VBC/ButtonBox/ScrollContainer/DoorbellList
+
+func setup(p_stage_mngr: EMC_StageMngr) -> void:
+	#await ready
+	
+	#Load doorbell data
+	#TODO: Load from JSON
+	var doorbell_agathe := _DOORBELL_SCN.instantiate()
+	doorbell_agathe.setup("Agathe", EMC_Action.IDs.SC_MARKET) #TODO update stage name
+	doorbell_agathe.rang.connect(p_stage_mngr._on_doorbell_rang)
+	_doorbell_list.add_child(doorbell_agathe)
+	
+	var doorbell_mert := _DOORBELL_SCN.instantiate()
+	doorbell_mert.setup("Mert", EMC_Action.IDs.SC_MARKET)  #TODO, connect a stage to doorbellBtn
+	doorbell_mert.rang.connect(p_stage_mngr._on_doorbell_rang)
+	_doorbell_list.add_child(doorbell_mert)
+	
+	var doorbell_veronika := _DOORBELL_SCN.instantiate()
+	doorbell_veronika.setup("Veronika & Kris", EMC_Action.IDs.SC_MARKET)  #TODO, connect a stage to doorbellBtn
+	doorbell_veronika.rang.connect(p_stage_mngr._on_doorbell_rang)
+	_doorbell_list.add_child(doorbell_veronika)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	hide()
+
+
+
+func open() -> void:
+	show()
+	#opened.emit()
+
+
+func close() -> void:
+	hide()
+	#closed.emit()
