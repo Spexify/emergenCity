235a67033b9a214b8480bf6b3a00999577294687
cccynic <karina.miroslavova.lazarova@gmail.com>
2024-02-17 17:52:13 +0100

    Credits and Information v2
    information filled in, links connected

--- godot/preparePhase/credit_information.gd
+++ godot/preparePhase/credit_information.gd
@@ -11,3 +11,11 @@ func close() -> void:
 func _on_texture_button_pressed() -> void:
 	hide()
 	Global.goto_scene("res://preparePhase/main_menu.tscn")
+
+
+func _on_cooking_button_pressed() -> void:
+	OS.shell_open("https://www.bbk.bund.de/DE/Warnung-Vorsorge/Tipps-Notsituationen/Kochen-ohne-Strom/kochen-ohne-strom_node.html")
+
+
+func _on_bbk_button_pressed() -> void:
+	OS.shell_open("https://www.bbk.bund.de/SharedDocs/Downloads/DE/Mediathek/Publikationen/Buergerinformationen/Ratgeber/ratgeber-notfallvorsorge.pdf?__blob=publicationFile&v=15")
