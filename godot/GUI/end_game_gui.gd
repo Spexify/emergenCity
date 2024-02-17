8d3cfdd2278646360f219bfdcd8a8a02ad0a655c
cccynic <karina.miroslavova.lazarova@gmail.com>
2024-02-17 15:46:54 +0100

    EG user study feedback
    there is a reason for losing shown to the USER

--- godot/GUI/end_game_gui.gd
+++ godot/GUI/end_game_gui.gd
@@ -17,7 +17,7 @@ var history : Array [EMC_DayCycle]
 		#close()
 
 ## opens summary end of day GUI/makes visible
-func open(p_history: Array[EMC_DayCycle], p_avatar_life_status : bool, p_happinness_status  : int) -> void:
+func open(p_history: Array[EMC_DayCycle], p_avatar_life_status : bool, _avatar_ref : EMC_Avatar) -> void:
 	
 	$WinnerScreen/MarginContainer/VBoxContainer/TextBox3/MarginContainer/ScrollContainer.vertical_scroll_mode = true
 	history = p_history
@@ -75,23 +75,34 @@ func open(p_history: Array[EMC_DayCycle], p_avatar_life_status : bool, p_happinn
 						 " Mal ausgeführt .\n"
 						
 	if p_avatar_life_status == false :
+		var losing_reason : String = ""
+		if _avatar_ref.get_nutrition_status() == 0:
+			losing_reason = "Du bist verhungert. "
+		if _avatar_ref.get_hydration_status() == 0:
+			losing_reason = "Du bist verdurstet. "
+		if _avatar_ref.get_health_status() == 0:
+			losing_reason = "Du hattest Gesundheitsprobleme. "
+			
 		$LoserScreen/MarginContainer/VBoxContainer/TextBox3/ScrollContainer/Actions.text \
 			= summary_text_loser
 		$LoserScreen/MarginContainer/VBoxContainer/TextBox2/Description.text =\
-				"Du hast 100 ECoins erworben!"
+				losing_reason + "Du hast nur 100 ECoins erworben."
 		$LoserScreen.visible = true
 		$WinnerScreen.visible = false
 		Global.set_e_coins(Global.get_e_coins() + 100)
 	else: 
+		
 		$WinnerScreen/MarginContainer/VBoxContainer/TextBox3/MarginContainer/ScrollContainer/Actions.text \
 			= summary_text_winner
-		all_action_coins += p_happinness_status
+			
+		all_action_coins += _avatar_ref.get_happinness_status()
+		Global.set_e_coins(Global.get_e_coins() + all_action_coins)
 		$WinnerScreen/MarginContainer/VBoxContainer/TextBox/Description.text =\
 				"Du hast " + str(all_action_coins) + " ECoins erworben!"
 		all_action_coins = 0
+				
 		$LoserScreen.visible = false
 		$WinnerScreen.visible = true
-		Global.set_e_coins(Global.get_e_coins() + all_action_coins)
 		open_gui_sfx.play()
 	visible = true
 	opened.emit()
