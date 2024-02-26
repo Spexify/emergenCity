c427b0b31899605d724eac3dbb5a93e39bea2f07
cccynic <karina.miroslavova.lazarova@gmail.com>
2024-02-26 13:31:54 +0100

    Intro v2.2
    done
    TODO : polish and avatar save

--- godot/preparePhase/main_menu.gd
+++ godot/preparePhase/main_menu.gd
@@ -87,4 +87,5 @@ func _on_avatar_selection_gui_closed() -> void:
 							3, 1, "Der Strom ist ausgefallen!")
 	var start_scene_name : String = Global.CRISIS_PHASE_SCENE
 	Global.goto_scene(start_scene_name)
+	
 	close()
