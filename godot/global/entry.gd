8de9b15edd0d027ad360fedfc56f3e5ce8ae0189
jakob huth <jakob.huth2003@gmail.com>
2024-03-05 16:05:55 +0100

    Code Review
    Entry

--- godot/global/entry.gd
+++ godot/global/entry.gd
@@ -5,10 +5,6 @@ func _ready() -> void:
 	JsonMngr.load_pop_up_actions()
 	JsonMngr.load_opt_events()
 	JsonMngr.load_actions()
-	#if !Global._tutorial_done:
-		### add animation, avtar setting and being thrown in the middle of an easy crisis
-		#Global.goto_scene(Global.FIRST_GAME_SCENE)
-	#else:
 	Global.load_game()
 	var start_scene_name : String = Global.load_scene_name()
 	Global.goto_scene(start_scene_name)
