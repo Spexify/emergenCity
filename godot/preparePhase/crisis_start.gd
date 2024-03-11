328cb40acb56c68bfea76747b7167c034c3cf6e7
cccynic <karina.miroslavova.lazarova@gmail.com>
2024-03-11 12:43:26 +0100

    issue #129
    text is clickable, same function as slider

--- godot/preparePhase/crisis_start.gd
+++ godot/preparePhase/crisis_start.gd
@@ -22,14 +22,11 @@ var _scenario : EMC_CrisisScenario
 func _on_continue_pressed() -> void:
 	_rng.randomize()
 	if $CanvasLayer/VBoxContainer/VBoxContainer/HSlider.value == 0:
-		_crisis_length = _rng.randi_range(LENGTH_LOWER_BOUND_EASY, LENGTH_UPPER_BOUND_EASY)
-		_number_crisis_overlap = _rng.randi_range(CRISIS_OVERLAP_LOWER_BOUND, CRISIS_OVERLAP_UPPER_BOUND)
+		_on_easy_pressed()
 	elif $CanvasLayer/VBoxContainer/VBoxContainer/HSlider.value == 1:
-		_crisis_length = _rng.randi_range(LENGTH_LOWER_BOUND_NORMAL, LENGTH_UPPER_BOUND_NORMAL)
-		_number_crisis_overlap = _rng.randi_range(CRISIS_OVERLAP_LOWER_BOUND, CRISIS_OVERLAP_UPPER_BOUND)
+		_on_normal_pressed()
 	elif $CanvasLayer/VBoxContainer/VBoxContainer/HSlider.value == 2:
-		_crisis_length = _rng.randi_range(LENGTH_LOWER_BOUND_HARD, LENGTH_UPPER_BOUND_HARD)
-		_number_crisis_overlap = _rng.randi_range(CRISIS_OVERLAP_LOWER_BOUND, CRISIS_OVERLAP_UPPER_BOUND)
+		_on_hard_pressed()
 	
 	var _current_scenario := EMC_CrisisScenario.new() #TODO: Load scenario & its state from savefile..
 	
@@ -41,3 +38,18 @@ func _on_continue_pressed() -> void:
 		OverworldStatesMngr.set_crisis_difficulty()
 		
 	Global.goto_scene(Global.CRISIS_PHASE_SCENE)
+
+
+func _on_easy_pressed() -> void:
+		_crisis_length = _rng.randi_range(LENGTH_LOWER_BOUND_EASY, LENGTH_UPPER_BOUND_EASY)
+		_number_crisis_overlap = _rng.randi_range(CRISIS_OVERLAP_LOWER_BOUND, CRISIS_OVERLAP_UPPER_BOUND)
+
+
+func _on_normal_pressed() -> void:
+		_crisis_length = _rng.randi_range(LENGTH_LOWER_BOUND_NORMAL, LENGTH_UPPER_BOUND_NORMAL)
+		_number_crisis_overlap = _rng.randi_range(CRISIS_OVERLAP_LOWER_BOUND, CRISIS_OVERLAP_UPPER_BOUND)
+
+
+func _on_hard_pressed() -> void:
+		_crisis_length = _rng.randi_range(LENGTH_LOWER_BOUND_HARD, LENGTH_UPPER_BOUND_HARD)
+		_number_crisis_overlap = _rng.randi_range(CRISIS_OVERLAP_LOWER_BOUND, CRISIS_OVERLAP_UPPER_BOUND)
