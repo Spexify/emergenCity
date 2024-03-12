0ccbff3fa95ed5fb8baa2ddf8c6e5ccb37cbf26b
Ole Fabritz <o.fabritz@web.de>
2024-03-12 17:57:35 +0100

    Main menu & Upgrade center renaming of Btns and Title

--- godot/preparePhase/upgrade_center.gd
+++ godot/preparePhase/upgrade_center.gd
@@ -24,7 +24,6 @@ func _ready() -> void:
 	_add_balance(0)
 	
 	_equipped_upgrades = Global.get_upgrades()
-	print(_equipped_upgrades)
 	
 	for id : EMC_Upgrade.IDs in EMC_Upgrade.IDs.values():
 		var _added_upgrade : EMC_Upgrade = null
@@ -155,6 +154,5 @@ func _on_main_menu_btn_pressed() -> void:
 		up.setup(_equipped_upgrades[i].get_id())
 		tmp.append(up)
 	
-	print(tmp)
 	Global.set_upgrades(tmp)
 	Global.goto_scene(Global.MAIN_MENU_SCENE)
