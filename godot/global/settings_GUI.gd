c6034af326505113e4b07059c514d777c93e0da9
Jakob Huth <jakob.huth2003@gmail.com>
2025-10-18 13:40:21 +0200

    Export for PlayStore + Versioning
    Change to use Gradle build for PlayStore + Change version to start at 0 as Krisopolis does not yet have a truly release Version

--- godot/global/settings_GUI.gd
+++ godot/global/settings_GUI.gd
@@ -136,4 +136,4 @@ func _on_vibrate_pressed() -> void:
 
 
 func _on_info_pressed() -> void:
-	tooltip_gui.open("Godot version 4.4.1\nGame Version 1.3.4 - Berlin")
+	tooltip_gui.open("Godot version 4.4.1\nGame Version 0.3.4")
